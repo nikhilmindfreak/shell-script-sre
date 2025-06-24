0ca4e79bcb8bec880debdbabc5181b8657364c7c
nikhilmindfreak <nikhilbabayolla@gmail.com>
2025-06-24 17:59:29 -0400

    02

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,5 @@
-#! bin bash
-sh #!/bin/bash
+#!/bin/bash
+
+PERSON="John Doe" # NO spaces allowed in variable names 
+echo "Hello, $PERSON!"  # $ is used to access the variable
+echo "This script demonstrates the use of variables in shell scripting."
