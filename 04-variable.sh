91adbdf121e39b8c75d3e7098f6679684b27b99f
nikhilmindfreak <nikhilbabayolla@gmail.com>
2025-06-24 18:10:55 -0400

    02

--- 04-variable.sh
+++ 04-variable.sh
@@ -2,8 +2,8 @@
 
 # we use read to get input from the user
 echo "Enter your user name: "
-read USERNAME
+read -s USERNAME   #-s option is used to hide the input for security reasons
 echo "please enter your password: "
-read PASSWORD  
+read -s PASSWORD  
 echo "Hello, $USERNAME!, your password is $PASSWORD"
 echo "This script demonstrates the use of variables in shell scripting."
