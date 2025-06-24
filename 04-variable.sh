b35753694a400c7de507202e57517b325a877133
nikhilmindfreak <nikhilbabayolla@gmail.com>
2025-06-24 18:08:58 -0400

    02

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash         
+
+# we use read to get input from the user
+echo "Enter your user name: "
+read USERNAME
+echo "please enter your password: "
+read PASSWORD  
+echo "Hello, $USERNAME!, your password is $PASSWORD"
+echo "This script demonstrates the use of variables in shell scripting."
